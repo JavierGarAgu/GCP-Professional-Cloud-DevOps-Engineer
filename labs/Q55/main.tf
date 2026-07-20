05cdb307e4bed8b77b3b2fd0acf3de3b2a588282
javierGarAgu <javiergaragu@gmail.com>
2026-07-20 20:21:47 +0200

    adding cloudbuild basic setup explanation

--- labs/Q55/main.tf
+++ labs/Q55/main.tf
@@ -98,17 +98,53 @@ resource "google_storage_bucket" "source_bucket" {
 
 }
 
+resource "google_service_account" "cloudbuild_sa" {
+
+  account_id   = "container-analysis-cloudbuild"
+  display_name = "Container Analysis Cloud Build"
+
+}
+
+#######################################################
+# LOGGING WRITER
+#######################################################
+
+resource "google_project_iam_member" "cloudbuild_logging" {
+
+  project = "devops-cert-labs-v2"
+
+  role = "roles/logging.logWriter"
+
+  member = "serviceAccount:${google_service_account.cloudbuild_sa.email}"
+
+}
+
+#######################################################
+# ARTIFACT REGISTRY WRITER
+#######################################################
+
+resource "google_project_iam_member" "cloudbuild_artifact_writer" {
+
+  project = "devops-cert-labs-v2"
+
+  role = "roles/artifactregistry.writer"
+
+  member = "serviceAccount:${google_service_account.cloudbuild_sa.email}"
+
+}
+
 #######################################################
 # CLOUD BUILD TRIGGER
 #######################################################
 resource "google_cloudbuild_trigger" "container_pipeline" {
 
+  service_account = google_service_account.cloudbuild_sa.id
   depends_on = [
     google_project_service.cloudbuild,
     google_artifact_registry_repository.repository
   ]
 
-  name        = "container-analysis-pipeline"
+  name = "container-analysis-pipeline"
 
   description = "Automatically builds and pushes container images."
 
